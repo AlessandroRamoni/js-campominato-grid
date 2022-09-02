d3adaadd6b9de24817681b56bed334fa5fee7b6b
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-09-02 16:30:23 +0200

    create js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 console.log("Venerdì");
+
+const pulsante = document.getElementById("pulsante");
+
+pulsante.addEventListener("click", function () {
+  const grid = document.getElementById("grid");
+  grid.innerHTML = "";
+
+  for (let i = 1; i < 101; i++) {
+    const cella = document.createElement("div");
+    cella.className = "cell";
+    cella.innerHTML = i;
+
+    cella.addEventListener("click", function () {
+      console.log("stai cliccando sulla cella numero", i);
+      const isEven = isNumberEven(i);
+
+      if (isEven) {
+        cella.classList.add("even");
+      } else {
+        cella.classList.add("odd");
+      }
+    });
+
+    grid.append(cella);
+  }
+});
+
+function isNumberEven(number) {
+  return number % 2 === 0;
+}
